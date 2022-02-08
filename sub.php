b911700521352f6b23a17534098eab6c3b26f86f
Abhirami p kumar_kinghuzzzz <97221313+AbhiramiP123@users.noreply.github.com>
2022-02-08 03:48:55 -0800

    php program to performsubtractionofgiventwonumbers

---
+++ sub.php
@@ -0,0 +1,14 @@
+<?php
+function add($a,$b)
+{
+$sum=$a+$b;
+echo "Sum of two numbers is =$sum <br>";
+}
+add(465,943);
+function sub($a,$b)
+{
+$diff=$a-$b;
+echo "Difference of two numbers is =$diff";
+}
+sub(943,465);
+?>
